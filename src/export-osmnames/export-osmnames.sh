b602bbab4da6b86e1c1de0bddc84741973ac5803
and01 <and01@users.noreply.github.com>
2016-05-10 11:00:47 +0200

    #19 fixed

--- src/export-osmnames/export-osmnames.sh
+++ src/export-osmnames/export-osmnames.sh
@@ -7,6 +7,9 @@ readonly DB_HOST=$DB_PORT_5432_TCP_ADDR
 readonly DB_PORT=$DB_PORT_5432_TCP_PORT
 readonly EXPORT_DIR=${EXPORT_DIR:-"/data/"}
 
+readonly IMPORT_DATA_DIR=${IMPORT_DATA_DIR:-/data}
+
+
 function export_tsv() {
     local tsv_filename="$1"
     local tsv_file="$EXPORT_DIR/$tsv_filename"
@@ -29,9 +32,20 @@ function gzip_tsv() {
     gzip -c "$tsv_file" > "$tsv_file.gzip"
 }
 
+function determineOutputFilename() {
+    if [ "$(ls -A $IMPORT_DATA_DIR/*.pbf 2> /dev/null)" ]; then
+    filename=$(ls -t -U $IMPORT_DATA_DIR/**.pbf | xargs -n1 basename | sed -e 's/\..*$//')
+    echo "$filename"
+    else
+        echo "output"
+    fi
+    return
+}
+
 function export_geonames() {
-    export_tsv "output.tsv" "export.sql"
-    gzip_tsv "output.tsv"
+    filename=$(determineOutputFilename)
+    export_tsv "$filename.tsv" "export.sql"
+    gzip_tsv "$filename.tsv"
 }
 
 export_geonames
