4f47e6aaff40a83ecdb8e181408d1402aa235a61
and01 <and01@users.noreply.github.com>
2016-07-18 10:05:52 +0200

    fixed bug causing TopologyException with some polygons by adding ST_Buffer

--- src/export-osmnames/01_polygons.sql
+++ src/export-osmnames/01_polygons.sql
@@ -19,4 +19,13 @@ SELECT getLanguageName(r.name, r.name_fr, r.name_en, r.name_de, r.name_es, r.nam
     ST_YMAX(ST_Transform(r.geometry, 4326)) AS north,
     getWikipediaURL(r.wikipedia, r.calculated_country_code) AS wikipedia
 FROM osm_polygon AS r , getParentInfo(getLanguageName(name, name_fr, name_en, name_de, name_es, name_ru, name_zh), parent_id, rank_search, ',') AS parentInfo
+WHERE ST_IsValid(r.geometry) IS TRUE
 ;
+
+CREATE OR REPLACE FUNCTION geomInfo(geom geometry)
+RETURNS geometry AS $$
+BEGIN
+  RAISE NOTICE 'geom info %', ST_ASText(geom);
+  RETURN geom;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
