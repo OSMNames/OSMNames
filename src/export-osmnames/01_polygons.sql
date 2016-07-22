3a307e6ec57472aeb18f81e0ba799cbac20eca2d
and01 <and01@users.noreply.github.com>
2016-07-22 18:54:56 +0200

    fixed bug with ST_Buffer

--- src/export-osmnames/01_polygons.sql
+++ src/export-osmnames/01_polygons.sql
@@ -2,8 +2,8 @@ CREATE MATERIALIZED VIEW mv_polygons AS
 SELECT getLanguageName(r.name, r.name_fr, r.name_en, r.name_de, r.name_es, r.name_ru, r.name_zh) AS name,
     city_class(r.type) AS class,
     r.type AS type,
-    ST_X(ST_PointOnSurface(ST_Transform(r.geometry, 4326))) AS lon,
-    ST_Y(ST_PointOnSurface(ST_Transform(r.geometry, 4326))) AS lat,
+    ST_X(ST_PointOnSurface(ST_Buffer(ST_Transform(r.geometry, 4326),0.0))) AS lon,
+    ST_Y(ST_PointOnSurface(ST_Buffer(ST_Transform(r.geometry, 4326),0.0))) AS lat,
     r.rank_search AS place_rank,
     getImportance(r.rank_search, r.wikipedia, r.calculated_country_code) AS importance,
     ''::TEXT AS street,
@@ -19,13 +19,4 @@ SELECT getLanguageName(r.name, r.name_fr, r.name_en, r.name_de, r.name_es, r.nam
     ST_YMAX(ST_Transform(r.geometry, 4326)) AS north,
     getWikipediaURL(r.wikipedia, r.calculated_country_code) AS wikipedia
 FROM osm_polygon AS r , getParentInfo(getLanguageName(name, name_fr, name_en, name_de, name_es, name_ru, name_zh), parent_id, rank_search, ',') AS parentInfo
-WHERE ST_IsValid(r.geometry) IS TRUE
 ;
-
-CREATE OR REPLACE FUNCTION geomInfo(geom geometry)
-RETURNS geometry AS $$
-BEGIN
-  RAISE NOTICE 'geom info %', ST_ASText(geom);
-  RETURN geom;
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
