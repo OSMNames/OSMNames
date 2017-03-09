cf5d1620c975e68ade8f83f26313d49dd8a10885
Philipp Koster <philipp.koster@startmail.com>
2017-03-09 23:50:31 +0100

    skip pbf download for now, if file already exists

--- src/download-pbf/download-pbf.sh
+++ src/download-pbf/download-pbf.sh
@@ -3,8 +3,13 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-readonly FILE_URL="http://download.geofabrik.de/europe/switzerland-latest.osm.pbf"
+readonly FILE_NAME="switzerland-latest.osm.pbf"
+readonly FILE_URL="http://download.geofabrik.de/europe/${FILE_NAME}"
 readonly EXPORT_DIR="${DATA_DIR}/export/"
 
-echo "$(date +"%T"): downloading file.."
-wget $FILE_URL -P $EXPORT_DIR
+if [ ! -f "$EXPORT_DIR/$FILE_NAME" ]; then
+  echo "$(date +"%T"): downloading file.."
+  wget $FILE_URL -P $EXPORT_DIR
+else
+  echo "download of $FILE_NAME skipped, since it exists"
+fi
