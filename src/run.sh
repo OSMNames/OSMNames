180a6573653c64df16a68af25f6b03ba1d8cba3c
Philipp Koster <philipp.koster@startmail.com>
2017-03-10 16:09:15 +0100

    improved wait for postgresql block

--- src/run.sh
+++ src/run.sh
@@ -3,10 +3,9 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-TIMEOUT=5
-until psql postgres -c "select 1" > /dev/null 2>&1 || [ $TIMEOUT -eq 0 ]; do
-  echo "Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
-  sleep 3
+until psql --username=postgres postgres -c "select 1" > /dev/null 2>&1; do
+  echo "Waiting for postgres server"
+  sleep 2
 done
 
 . './shared/functions.sh'
