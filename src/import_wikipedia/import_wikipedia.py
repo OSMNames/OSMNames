5ade6441c1bc2475022be34928cc603b92917583
Philipp Koster <philipp.koster@startmail.com>
2017-03-19 17:42:15 +0100

    pythorized import_wikipedia

---
+++ src/import_wikipedia/import_wikipedia.py
@@ -0,0 +1,50 @@
+import os
+from subprocess import check_call
+from shared.helpers import exec_sql, psql_exec
+
+
+def run():
+    download_wikipedia_dump()
+    restore_wikipedia_dump()
+    create_wikipedia_index()
+
+    psql_exec("create_index.sql", cwd=os.path.dirname(__file__))
+
+
+def download_wikipedia_dump():
+    url = os.getenv("WIKIPEDIA_DUMP_URL")
+    destination_dir = os.getenv("IMPORT_DIR")
+    check_call(["wget", "--no-clobber", "--directory-prefix", destination_dir, url])
+
+
+def restore_wikipedia_dump():
+    _create_temporary_user_for_dump()
+
+    dump_filename = os.getenv("WIKIPEDIA_DUMP_URL").split("/")[-1]
+    dump_path = "{}/{}".format(os.getenv("IMPORT_DIR"), dump_filename)
+
+    check_call(["pg_restore", "--dbname", "osm", "-U", "brian", dump_path])
+
+    _drop_temporary_user_for_dump()
+
+
+def _create_temporary_user_for_dump():
+    query = """
+        CREATE ROLE brian LOGIN PASSWORD 'brian';
+        GRANT ALL PRIVILEGES ON DATABASE {database} to brian;
+    """.format(database=os.getenv("DB_NAME"))
+
+    exec_sql(query, user="postgres")
+
+
+def _drop_temporary_user_for_dump():
+    query = """
+        ALTER TABLE wikipedia_article OWNER TO {username};
+        DROP ROLE brian;
+    """.format(username=os.getenv("DB_USER"))
+
+    exec_sql(query, user="postgres")
+
+
+def create_wikipedia_index():
+    exec_sql("CREATE INDEX idx_wikipedia_article_language_title ON wikipedia_article (language,title);")
