d975238724750d062e7e8ac2757acf5893a48e7d
Philipp Koster <philipp.koster@startmail.com>
2017-04-13 12:51:24 +0200

    added a logger

--- osmnames/helpers/database.py
+++ osmnames/helpers/database.py
@@ -1,6 +1,10 @@
 import psycopg2
+
 from subprocess import check_call
 from osmnames import settings
+from osmnames import logger
+
+log = logger.setup(__name__)
 
 
 def psql_exec(filepath, user=settings.get("DB_USER"), cwd=""):
@@ -17,7 +21,9 @@ def exec_sql_from_file(filename, user=settings.get("DB_USER"), database=settings
     file_path = "{}/{}".format(cwd, filename)
     connection = psycopg2.connect(user=user, dbname=database)
     connection.set_session(autocommit=True)
+    log.info("start executing sql file {}".format(filename))
     connection.cursor().execute(open(file_path, "r").read())
+    log.info("finished executing sql file {}".format(filename))
 
 
 def exec_sql(sql, user=settings.get('DB_USER'), database=settings.get('DB_NAME')):
---
+++ osmnames/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def setup(name):
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    return logger
