9e9d2cfb13f1487805ed2df8f141b3e8c4df6cf4
Philipp Koster <philipp.koster@startmail.com>
2017-04-25 09:14:52 +0200

    better naming for set_country_code function

--- osmnames/import_osm/set_country_codes.sql
+++ osmnames/import_osm/set_country_codes.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION IF EXISTS set_country_code_for_containing_elements(VARCHAR(2), geometry);
-CREATE FUNCTION set_country_code_for_containing_elements(country_code_in VARCHAR(2), geometry_value GEOMETRY) RETURNS VOID AS $$
+DROP FUNCTION IF EXISTS set_country_code_for_elements_within_geometry(VARCHAR(2), geometry);
+CREATE FUNCTION set_country_code_for_elements_within_geometry(country_code_in VARCHAR(2), geometry_value GEOMETRY) RETURNS VOID AS $$
 BEGIN
   UPDATE osm_linestring SET country_code = country_code_in WHERE country_code = '' IS NOT FALSE
                                                                  AND st_contains(geometry_value, geometry);
@@ -22,12 +22,12 @@ BEGIN
   UPDATE osm_polygon SET country_code = lower(imported_country_code) WHERE imported_country_code IS NOT NULL;
 
   -- use country grid to set country codes for containing elements
-  PERFORM set_country_code_for_containing_elements(lower(country_code), geometry)
+  PERFORM set_country_code_for_elements_within_geometry(lower(country_code), geometry)
           FROM country_osm_grid
           ORDER BY area ASC;
 
   -- finally use polygons with highest admin_levels for remaining elements without country_code
-  PERFORM set_country_code_for_containing_elements(lower(country_code), geometry)
+  PERFORM set_country_code_for_elements_within_geometry(lower(country_code), geometry)
           FROM osm_polygon
           WHERE admin_level <= 4;
 END
