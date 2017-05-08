774a9c0ab57f83226f3afddcf2dddb4e2e822f2c
Philipp Koster <philipp.koster@startmail.com>
2017-05-08 10:45:22 +0200

    do not consider country_code when creating hierarchy

--- osmnames/import_osm/create_hierarchy.sql
+++ osmnames/import_osm/create_hierarchy.sql
@@ -1,25 +1,22 @@
-DROP FUNCTION IF EXISTS set_parent_id_for_elements_within_geometry(BIGINT, INT, VARCHAR(2), geometry);
-CREATE FUNCTION set_parent_id_for_elements_within_geometry(id_in BIGINT, admin_level_in INT, country_code_in VARCHAR(2), geometry_in GEOMETRY) RETURNS VOID AS $$
+DROP FUNCTION IF EXISTS set_parent_id_for_elements_within_geometry(BIGINT, INT, geometry);
+CREATE FUNCTION set_parent_id_for_elements_within_geometry(id_in BIGINT, admin_level_in INT, geometry_in GEOMETRY)
+RETURNS VOID AS $$
 BEGIN
   UPDATE osm_linestring SET parent_id = id_in WHERE parent_id IS NULL
                                                     AND id_in != id
-                                                    AND country_code = country_code_in
                                                     AND st_contains(geometry_in, geometry);
 
   UPDATE osm_polygon SET parent_id = id_in WHERE parent_id IS NULL
                                                  AND id_in != id
                                                  AND admin_level > admin_level_in
-                                                 AND country_code = country_code_in
                                                  AND st_contains(geometry_in, geometry);
 
   UPDATE osm_housenumber SET parent_id = id_in WHERE parent_id IS NULL
                                                      AND id_in != id
-                                                     AND country_code = country_code_in
                                                      AND st_contains(geometry_in, geometry);
 
   UPDATE osm_point SET parent_id = id_in WHERE parent_id IS NULL
                                                AND id_in != id
-                                               AND country_code = country_code_in
                                                AND st_contains(geometry_in, geometry)
                                                AND linked IS FALSE;
 END;
@@ -34,7 +31,7 @@ CREATE INDEX IF NOT EXISTS idx_osm_polygon_admin_level ON osm_polygon(admin_leve
 
 DO $$
 BEGIN
-  PERFORM set_parent_id_for_elements_within_geometry(id, admin_level, country_code, geometry)
+  PERFORM set_parent_id_for_elements_within_geometry(id, admin_level, geometry)
           FROM osm_polygon
           WHERE place_rank <= 22
             AND type IN ('administrative', 'continent', 'country', 'state',
