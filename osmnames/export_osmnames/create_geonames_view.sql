8ffa11043aead2d8e809b616141ef7778ff505a3
Philipp Koster <philipp.koster@startmail.com>
2017-05-22 13:01:22 +0200

    better names for exported files, fixes #87

---
+++ osmnames/export_osmnames/create_geonames_view.sql
@@ -0,0 +1,8 @@
+CREATE VIEW geonames_view AS
+  SELECT * FROM mv_polygons
+  UNION ALL
+  SELECT * FROM mv_points
+  UNION ALL
+  SELECT * FROM mv_linestrings
+  UNION ALL
+  SELECT * FROM mv_merged_linestrings;
--- osmnames/export_osmnames/export.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM mv_polygons
-UNION ALL
-SELECT * FROM mv_points
-UNION ALL
-SELECT * FROM mv_linestrings
-UNION ALL
-SELECT * FROM mv_merged_linestrings;
