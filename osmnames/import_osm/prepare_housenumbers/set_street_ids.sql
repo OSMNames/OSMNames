646fc07b087a36bd4b10b2aba5126d2db12669fa
Philipp Koster <philipp.koster@startmail.com>
2017-07-13 17:35:40 +0200

    housenumbers are now associated to streets via relations

--- osmnames/import_osm/prepare_housenumbers/set_street_ids.sql
+++ osmnames/import_osm/prepare_housenumbers/set_street_ids.sql
@@ -2,4 +2,5 @@ UPDATE osm_housenumber AS housenumber
   SET street_id = COALESCE(street.merged_into, street.id)
 FROM osm_linestring AS street
 WHERE (street.parent_id = housenumber.parent_id OR housenumber.parent_id = ANY(street.intersecting_polygon_ids))
-      AND street.name = housenumber.street;
+      AND street.name = housenumber.street
+      AND housenumber.street_id IS NULL;
