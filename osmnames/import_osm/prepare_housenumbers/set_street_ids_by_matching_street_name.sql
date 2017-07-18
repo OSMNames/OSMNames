34506f9cbf664ab0a3294c7b4a7049093ceba43d
Philipp Koster <philipp.koster@startmail.com>
2017-07-18 09:38:59 +0200

    create indexes for set_street_ids_by_matching_street_names

--- osmnames/import_osm/prepare_housenumbers/set_street_ids_by_matching_street_name.sql
+++ osmnames/import_osm/prepare_housenumbers/set_street_ids_by_matching_street_name.sql
@@ -1,6 +1,12 @@
+CREATE INDEX IF NOT EXISTS idx_osm_housenumber_street ON osm_housenumber(street);
+CREATE INDEX IF NOT EXISTS idx_osm_linestring_name ON osm_linestring(name);
+
 UPDATE osm_housenumber AS housenumber
   SET street_id = COALESCE(street.merged_into, street.osm_id)
 FROM osm_linestring AS street
 WHERE (street.parent_id = housenumber.parent_id OR housenumber.parent_id = ANY(street.intersecting_polygon_ids))
       AND street.name = housenumber.street
       AND housenumber.street_id IS NULL;
+
+DROP INDEX idx_osm_housenumber_street;
+DROP INDEX idx_osm_linestring_name;
