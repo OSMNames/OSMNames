ea4b00f0e14bf0c2b5c474168afeac4a28c05eba
Philipp Koster <philipp.koster@startmail.com>
2017-07-18 09:38:19 +0200

    improved performance of set_names

--- osmnames/import_osm/set_names.sql
+++ osmnames/import_osm/set_names.sql
@@ -1,47 +1,41 @@
-DROP FUNCTION IF EXISTS get_alternative_names(HSTORE, TEXT);
-CREATE FUNCTION get_alternative_names(all_tags HSTORE, name TEXT)
-RETURNS TEXT AS $$
+DROP FUNCTION IF EXISTS get_names(TEXT, HSTORE);
+CREATE FUNCTION get_names(current_name TEXT, all_tags HSTORE)
+RETURNS TABLE(name TEXT, alternative_names_string TEXT) AS $$
 DECLARE
   accepted_name_tags TEXT[] := ARRAY['name:left','name:right','int_name','loc_name','nat_name',
                                      'official_name','old_name','reg_name','short_name','alt_name'];
   alternative_names TEXT[];
-  alternative_names_string TEXT;
 BEGIN
   SELECT array_agg(DISTINCT(all_tags -> key))
-  FROM unnest(akeys(all_tags)) AS key
-  WHERE key LIKE 'name:__' OR key = ANY(accepted_name_tags)
-  INTO alternative_names;
-  alternative_names := array_remove(alternative_names, name);
-  alternative_names_string := array_to_string(alternative_names, ',');
-  RETURN COALESCE(regexp_replace(alternative_names_string, E'\\s+', ' ', 'g'), '');
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
+    FROM unnest(akeys(all_tags)) AS key
+    WHERE key LIKE 'name:__' OR key = ANY(accepted_name_tags)
+    INTO alternative_names;
 
-
-DROP FUNCTION IF EXISTS get_name(HSTORE);
-CREATE FUNCTION get_name(all_tags HSTORE)
-RETURNS TEXT AS $$
-  SELECT COALESCE(
+  name := current_name;
+  IF name = '' IS NOT FALSE THEN
+    SELECT COALESCE(
                   all_tags -> 'name:en',
                   all_tags -> 'name:fr',
                   all_tags -> 'name:de',
                   all_tags -> 'name:es',
                   all_tags -> 'name:ru',
                   all_tags -> 'name:zh',
-                  split_part(get_alternative_names(all_tags, ''), ',', 1),
-                  ''
-                  );
-$$ LANGUAGE 'sql' IMMUTABLE;
+                  alternative_names[1],
+                  '')
+      INTO name;
+  END IF;
+  name := regexp_replace(name, E'\\s+', ' ', 'g');
+
+  alternative_names_string := 'test';
+  alternative_names := array_remove(alternative_names, name);
+  alternative_names_string := array_to_string(alternative_names, ',');
+  alternative_names_string := COALESCE(regexp_replace(alternative_names_string, E'\\s+', ' ', 'g'), '');
+
+  RETURN NEXT;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
 
-UPDATE osm_linestring SET name = get_name(all_tags) WHERE name = '' IS NOT FALSE;
-UPDATE osm_polygon SET name = get_name(all_tags) WHERE name = '' IS NOT FALSE;
-UPDATE osm_point SET name = get_name(all_tags) WHERE name = '' IS NOT FALSE;
-
-UPDATE osm_linestring SET alternative_names = get_alternative_names(all_tags, name);
-UPDATE osm_polygon SET alternative_names = get_alternative_names(all_tags, name);
-UPDATE osm_point SET alternative_names = get_alternative_names(all_tags, name);
-
-UPDATE osm_linestring SET name = regexp_replace(name, E'\\s+', ' ', 'g') WHERE name LIKE '%'||chr(9)||'%';
-UPDATE osm_polygon SET name = regexp_replace(name, E'\\s+', ' ', 'g') WHERE name LIKE '%'||chr(9)||'%';
-UPDATE osm_point SET name = regexp_replace(name, E'\\s+', ' ', 'g') WHERE name LIKE '%'||chr(9)||'%';
+UPDATE osm_linestring SET (name, alternative_names) = (SELECT * FROM get_names(name, all_tags));
+UPDATE osm_polygon SET (name, alternative_names) = (SELECT * FROM get_names(name, all_tags));
+UPDATE osm_point SET (name, alternative_names) = (SELECT * FROM get_names(name, all_tags));
