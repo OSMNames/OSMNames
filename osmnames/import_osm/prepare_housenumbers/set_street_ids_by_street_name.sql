e11ae98b1cb613bf1de5e2ac5144e6bd94331961
Philipp Koster <philipp.koster@startmail.com>
2017-07-29 15:27:25 +0200

    search street also based on range

--- osmnames/import_osm/prepare_housenumbers/set_street_ids_by_street_name.sql
+++ osmnames/import_osm/prepare_housenumbers/set_street_ids_by_street_name.sql
@@ -1,5 +1,6 @@
 CREATE INDEX IF NOT EXISTS osm_linestring_normalized_name ON osm_linestring(normalized_name);
 CREATE INDEX IF NOT EXISTS osm_linestring_normalized_name_trgm ON osm_linestring USING GIN(normalized_name gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS osm_linestring_geometry ON osm_linestring USING GIST(geometry);
 
 -- see https://www.postgresql.org/docs/9.6/static/pgtrgm.html for more information
 UPDATE pg_settings SET setting = '0.5' WHERE name = 'pg_trgm.similarity_threshold';
@@ -10,6 +11,17 @@ RETURNS BIGINT AS $$
   SELECT COALESCE(merged_into, osm_id)
     FROM osm_linestring
     WHERE parent_id = parent_id_in
+          AND normalized_name % name_in
+    ORDER BY similarity(normalized_name, name_in) DESC
+    LIMIT 1;
+$$ LANGUAGE 'sql' IMMUTABLE;
+
+DROP FUNCTION IF EXISTS best_matching_street_within_range(GEOMETRY, VARCHAR);
+CREATE FUNCTION best_matching_street_within_range(geometry_in GEOMETRY, name_in VARCHAR)
+RETURNS BIGINT AS $$
+  SELECT COALESCE(merged_into, osm_id)
+    FROM osm_linestring
+    WHERE st_dwithin(geometry_in, geometry, 1000)
           AND normalized_name % name_in
     ORDER BY similarity(normalized_name, name_in) DESC
     LIMIT 1;
@@ -31,5 +43,13 @@ UPDATE osm_housenumber
         AND normalized_street <> ''
         AND parent_id IS NOT NULL;
 
+-- set street id by best matching name within range
+UPDATE osm_housenumber
+  SET street_id = best_matching_street_within_range(geometry_center, normalized_street)
+  WHERE street_id IS NULL
+        AND normalized_street <> ''
+        AND parent_id IS NOT NULL;
+
 DROP INDEX osm_linestring_normalized_name;
 DROP INDEX osm_linestring_normalized_name_trgm;
+DROP INDEX osm_linestring_geometry;
