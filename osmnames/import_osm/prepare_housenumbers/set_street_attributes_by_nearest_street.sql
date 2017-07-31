d28379ed84de7b03d1f78248386bba511c4737a2
Philipp Koster <philipp.koster@startmail.com>
2017-07-31 12:35:59 +0200

    improved performance to set street ids by names

--- osmnames/import_osm/prepare_housenumbers/set_street_attributes_by_nearest_street.sql
+++ osmnames/import_osm/prepare_housenumbers/set_street_attributes_by_nearest_street.sql
@@ -6,15 +6,13 @@ BEGIN
   SELECT COALESCE(street.merged_into, street.osm_id), street.name
     FROM osm_linestring AS street
     WHERE parent_id = parent_id_in
+          AND st_dwithin(geometry, geometry_in, 1000)
     ORDER BY st_distance(geometry, geometry_in) ASC
     LIMIT 1;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
 
-CREATE INDEX idx_osm_linestring_parent_id ON osm_linestring(parent_id);
-
 UPDATE osm_housenumber
   SET (street_id, street) = (SELECT * FROM nearest_street(parent_id, geometry))
 WHERE street_id IS NULL
-      AND street = '' IS NOT FALSE
       AND parent_id IS NOT NULL;
--- osmnames/import_osm/prepare_housenumbers/set_street_ids_by_street_name.sql
+++ osmnames/import_osm/prepare_housenumbers/set_street_ids_by_street_name.sql
@@ -1,6 +1,7 @@
 CREATE INDEX IF NOT EXISTS osm_linestring_normalized_name ON osm_linestring(normalized_name);
 CREATE INDEX IF NOT EXISTS osm_linestring_normalized_name_trgm ON osm_linestring USING GIN(normalized_name gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS osm_linestring_geometry ON osm_linestring USING GIST(geometry);
+CREATE INDEX IF NOT EXISTS osm_housenumber_geometry_center ON osm_housenumber USING GIST(geometry_center);
 
 -- see https://www.postgresql.org/docs/9.6/static/pgtrgm.html for more information
 UPDATE pg_settings SET setting = '0.5' WHERE name = 'pg_trgm.similarity_threshold';
@@ -36,6 +37,15 @@ WHERE street.parent_id = housenumber.parent_id
       AND housenumber.street_id IS NULL
       AND housenumber.normalized_street != '';
 
+-- set street id by fully matching names within range
+UPDATE osm_housenumber AS housenumber
+  SET street_id = COALESCE(street.merged_into, street.osm_id)
+FROM osm_linestring AS street
+WHERE st_dwithin(street.geometry, housenumber.geometry_center, 1000)
+      AND street.normalized_name = housenumber.normalized_street
+      AND housenumber.street_id IS NULL
+      AND housenumber.normalized_street != '';
+
 -- set street id by best matching name within same parent
 UPDATE osm_housenumber
   SET street_id = best_matching_street_within_parent(parent_id, normalized_street)
@@ -52,4 +62,4 @@ UPDATE osm_housenumber
 
 DROP INDEX osm_linestring_normalized_name;
 DROP INDEX osm_linestring_normalized_name_trgm;
-DROP INDEX osm_linestring_geometry;
+DROP INDEX osm_housenumber_geometry_center;
