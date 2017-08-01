3aeaaf0683bacb1108f124f147496e68b4a931ab
Philipp Koster <philipp.koster@startmail.com>
2017-08-01 19:27:25 +0200

    commented best_matching_street_within_range due to terrible performance

--- osmnames/prepare_data/prepare_housenumbers/set_street_ids_by_street_name.sql
+++ osmnames/prepare_data/prepare_housenumbers/set_street_ids_by_street_name.sql
@@ -6,12 +6,13 @@ CREATE INDEX IF NOT EXISTS osm_housenumber_geometry_center ON osm_housenumber US
 -- see https://www.postgresql.org/docs/9.6/static/pgtrgm.html for more information
 UPDATE pg_settings SET setting = '0.5' WHERE name = 'pg_trgm.similarity_threshold';
 
-DROP FUNCTION IF EXISTS best_matching_street_within_parent(BIGINT, VARCHAR);
-CREATE FUNCTION best_matching_street_within_parent(parent_id_in BIGINT, name_in VARCHAR)
+DROP FUNCTION IF EXISTS best_matching_street_within_parent(BIGINT, GEOMETRY, VARCHAR);
+CREATE FUNCTION best_matching_street_within_parent(parent_id_in BIGINT, geometry_in GEOMETRY, name_in VARCHAR)
 RETURNS BIGINT AS $$
   SELECT COALESCE(merged_into, osm_id)
     FROM osm_linestring
     WHERE parent_id = parent_id_in
+          AND st_dwithin(geometry_in, geometry, 1000) -- added due better performance
           AND normalized_name % name_in
     ORDER BY similarity(normalized_name, name_in) DESC
     LIMIT 1;
@@ -48,17 +49,21 @@ WHERE st_dwithin(street.geometry, housenumber.geometry_center, 1000)
 
 -- set street id by best matching name within same parent
 UPDATE osm_housenumber
-  SET street_id = best_matching_street_within_parent(parent_id, normalized_street)
+  SET street_id = best_matching_street_within_parent(parent_id, geometry_center, normalized_street)
   WHERE street_id IS NULL
         AND normalized_street <> ''
         AND parent_id IS NOT NULL;
 
+
+-- This step is commented since it is to expensive to execute for a large amount of house numbers.
+-- Consider executing this step when less house numbers without street left (e.g. after Issue #109)
+--
 -- set street id by best matching name within range
-UPDATE osm_housenumber
-  SET street_id = best_matching_street_within_range(geometry_center, normalized_street)
-  WHERE street_id IS NULL
-        AND normalized_street <> ''
-        AND parent_id IS NOT NULL;
+-- UPDATE osm_housenumber
+--   SET street_id = best_matching_street_within_range(geometry_center, normalized_street)
+--   WHERE street_id IS NULL
+--         AND normalized_street <> ''
+--         AND parent_id IS NOT NULL;
 
 DROP INDEX osm_linestring_normalized_name;
 DROP INDEX osm_linestring_normalized_name_trgm;
--- tests/prepare_data/prepare_housenumbers/test_set_street_ids_by_street_name.py
+++ tests/prepare_data/prepare_housenumbers/test_set_street_ids_by_street_name.py
@@ -1,5 +1,7 @@
 import os
 import pytest
+
+from geoalchemy2.elements import WKTElement
 from osmnames.database.functions import exec_sql_from_file
 from osmnames.prepare_data.prepare_housenumbers import set_street_ids_by_street_name
 
@@ -55,8 +57,28 @@ def test_when_merged_street_with_same_parent_id_and_name_exists(session, schema,
 
 
 def test_when_street_with_same_parent_id_but_almost_same_name_exists(session, schema, tables):
-    session.add(tables.osm_housenumber(id=1, parent_id=1337, normalized_street="bochslensrasse"))
-    session.add(tables.osm_linestring(id=2, osm_id=42, parent_id=1337, normalized_name="bochslenstrasse"))
+    session.add(
+            tables.osm_housenumber(
+                id=1,
+                parent_id=1337,
+                osm_id=88267051,
+                normalized_street="bochslenrasse",
+                geometry_center=WKTElement("POINT(653642.999259018 6623211.41263188)", srid=3857)
+                )
+
+            )
+
+    session.add(
+            tables.osm_linestring(
+                id=2,
+                osm_id=42,
+                parent_id=1337,
+                normalized_name="bochslenstrasse",
+                geometry=WKTElement("""LINESTRING(653664.707113796
+                                       6623110.56066385,653621.291404239
+                                       6623312.26459991)""", srid=3857)
+                )
+            )
 
     session.commit()
 
@@ -66,8 +88,28 @@ def test_when_street_with_same_parent_id_but_almost_same_name_exists(session, sc
 
 
 def test_when_housenumber_street_contains_full_street_name(session, schema, tables):
-    session.add(tables.osm_housenumber(id=1, parent_id=1337, osm_id=88267051, normalized_street="citepreville19"))
-    session.add(tables.osm_linestring(id=2, osm_id=42, parent_id=1337, normalized_name="citepreville"))
+    session.add(
+            tables.osm_housenumber(
+                id=1,
+                parent_id=1337,
+                osm_id=88267051,
+                normalized_street="citepreville19",
+                geometry_center=WKTElement("POINT(653642.999259018 6623211.41263188)", srid=3857)
+                )
+
+            )
+
+    session.add(
+            tables.osm_linestring(
+                id=2,
+                osm_id=42,
+                parent_id=1337,
+                normalized_name="citepreville",
+                geometry=WKTElement("""LINESTRING(653664.707113796
+                                       6623110.56066385,653621.291404239
+                                       6623312.26459991)""", srid=3857)
+                )
+            )
 
     session.commit()
 
