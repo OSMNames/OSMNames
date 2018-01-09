a6951efcbc34c4699a0d4e6af428cb5c63c1cb2c
Philipp Koster <philipp.koster@startmail.com>
2018-01-09 12:40:03 +0100

    improved hierarchy for polygons

--- osmnames/prepare_data/create_hierarchy.py
+++ osmnames/prepare_data/create_hierarchy.py
@@ -10,6 +10,7 @@ log = logger.setup(__name__)
 
 def create_hierarchy():
     set_geometry_centers()
+    create_geometry_indexes()
     cluster_geometries()
 
     set_parent_ids()
@@ -21,6 +22,14 @@ def create_hierarchy():
 def set_geometry_centers():
     exec_sql_from_file("set_geometry_centers.sql", cwd=SQL_DIR)
     vacuum_database()
+
+
+def create_geometry_indexes():
+    exec_sql("""
+      CREATE INDEX osm_linestring_geometry_center ON osm_linestring USING gist(geometry_center);
+      CREATE INDEX osm_housenumber_geometry_center ON osm_housenumber USING gist(geometry_center);
+      CREATE INDEX osm_polygon_geometry ON osm_polygon USING gist(geometry);
+    """)
 
 
 def cluster_geometries():
@@ -36,6 +45,6 @@ def set_parent_ids():
 def drop_geometry_center_indexes():
     exec_sql("""
         DROP INDEX osm_linestring_geometry_center;
-        DROP INDEX osm_polygon_geometry_center;
         DROP INDEX osm_housenumber_geometry_center;
+        DROP INDEX osm_polygon_geometry;
     """)
