0d7dfdfe15b1fbc7f12b826dd21932d8fd103f0a
Philipp Koster <philipp.koster@startmail.com>
2018-05-04 12:52:08 +0200

    drop linestrings which are also present as polygons

--- osmnames/prepare_data/delete_unusable_entries.sql
+++ osmnames/prepare_data/delete_unusable_entries.sql
@@ -6,3 +6,6 @@ DELETE FROM osm_linestring WHERE name = '' IS NOT FALSE;
 
 --delete entries with faulty geometries from import
 DELETE FROM osm_polygon WHERE ST_IsEmpty(geometry);
+
+-- delete linestrings which are also polygons, see #162
+DELETE FROM osm_linestring WHERE osm_id in ANY(SELECT osm_id FROM osm_polygon);
