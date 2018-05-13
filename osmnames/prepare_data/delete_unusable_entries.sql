0855b5371acca8fcfb9dc5f7cb2eac41a4d35f87
Philipp Koster <philipp.koster@startmail.com>
2018-05-13 10:37:35 +0200

    fixed typo in query

--- osmnames/prepare_data/delete_unusable_entries.sql
+++ osmnames/prepare_data/delete_unusable_entries.sql
@@ -8,4 +8,4 @@ DELETE FROM osm_linestring WHERE name = '' IS NOT FALSE;
 DELETE FROM osm_polygon WHERE ST_IsEmpty(geometry);
 
 -- delete linestrings which are also polygons, see #162
-DELETE FROM osm_linestring WHERE osm_id in ANY(SELECT osm_id FROM osm_polygon);
+DELETE FROM osm_linestring WHERE osm_id = ANY(SELECT osm_id FROM osm_polygon);
