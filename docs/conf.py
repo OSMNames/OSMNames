f5e22a28a7829a732948411d145107c6530af921
Philipp K <philipp@nine.ch>
2019-02-24 14:59:13 +0100

    updated to python3

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'OSMNames'
+project = 'OSMNames'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'OSMNames.tex', u'OSMNames User Documentation',
-   u'', 'manual'),
+  ('index', 'OSMNames.tex', 'OSMNames User Documentation',
+   '', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'osmnames', u'OSMNames User Documentation',
-     [u''], 1)
+    ('index', 'osmnames', 'OSMNames User Documentation',
+     [''], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'OSMNames', u'OSMNames User Documentation',
-   u'', 'OSMNames', 'One line description of project.',
+  ('index', 'OSMNames', 'OSMNames User Documentation',
+   '', 'OSMNames', 'One line description of project.',
    'Miscellaneous'),
 ]
 
