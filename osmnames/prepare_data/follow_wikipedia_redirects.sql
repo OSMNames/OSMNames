f7f49d959ef6e62192e9cbfe4f3668306be4bac4
Philipp <philippks@protonmail.com>
2019-04-15 13:21:00 +0200

    consider wikipedia redirects for english fallback links

--- osmnames/prepare_data/follow_wikipedia_redirects.sql
+++ osmnames/prepare_data/follow_wikipedia_redirects.sql
@@ -1,14 +1,17 @@
 UPDATE osm_linestring
   SET wikipedia = to_title
   FROM wikipedia_redirect
-  WHERE wikipedia_redirect.from_title = wikipedia;
+  WHERE wikipedia_redirect.from_title = wikipedia
+    OR wikipedia_redirect.from_title = concat_ws(':', 'en', split_part(wikipedia, ':', 2)); --&
 
 UPDATE osm_point
   SET wikipedia = to_title
   FROM wikipedia_redirect
-  WHERE wikipedia_redirect.from_title = wikipedia;
+  WHERE wikipedia_redirect.from_title = wikipedia
+    OR wikipedia_redirect.from_title = concat_ws(':', 'en', split_part(wikipedia, ':', 2)); --&
 
 UPDATE osm_polygon
   SET wikipedia = to_title
   FROM wikipedia_redirect
-  WHERE wikipedia_redirect.from_title = wikipedia;
+  WHERE wikipedia_redirect.from_title = wikipedia
+    OR wikipedia_redirect.from_title = concat_ws(':', 'en', split_part(wikipedia, ':', 2)); --&
--- tests/prepare_data/test_follow_wikipedia_redirects.py
+++ tests/prepare_data/test_follow_wikipedia_redirects.py
@@ -27,3 +27,25 @@ def test_updates_with_redirects(session, tables):
 
     assert session.query(tables.osm_linestring).get(1).wikipedia == "en:redirection_destination"
     assert session.query(tables.osm_linestring).get(2).wikipedia == "en:should_not_be_redirected"
+
+
+def test_follows_english_redirects_as_fallback(session, tables):
+    session.add(
+            tables.osm_polygon(
+                id=1,
+                wikipedia="el:Ελλάδα"
+            )
+        )
+
+    session.add(
+            tables.wikipedia_redirect(
+                from_title="en:Ελλάδα",
+                to_title="en:Greece",
+            )
+        )
+
+    session.commit()
+
+    follow_wikipedia_redirects()
+
+    assert session.query(tables.osm_polygon).get(1).wikipedia == "en:Greece"
