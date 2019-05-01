26704bd0cb449b833abce29a6c044965b6902eb7
Philipp <philippks@protonmail.com>
2019-05-01 14:50:27 +0200

    update polygon type only when type = 'administrative'

--- osmnames/prepare_data/set_polygon_types.sql
+++ osmnames/prepare_data/set_polygon_types.sql
@@ -1,9 +1,10 @@
 -- The types of the polygons are already set from the imposm import.
 -- Here, libpostals fine-granular mapping between admin_level and type
--- is used to improve the quality, see issue #158
+-- is used to improve the quality for elements with the type 'administrative', see issue #158
 UPDATE osm_polygon AS polygon
   SET type = mapping.type
 FROM admin_level_type_mapping AS mapping
 WHERE mapping.country_code = polygon.country_code
   AND mapping.admin_level = polygon.admin_level
-  AND polygon.admin_level IS NOT NULL;
+  AND polygon.admin_level IS NOT NULL
+  AND polygon.type = 'administrative';
--- tests/prepare_data/test_set_polygon_types.py
+++ tests/prepare_data/test_set_polygon_types.py
@@ -9,3 +9,13 @@ def test_polygon_type_is_updated_when_mapping_exists(session, tables):
     set_polygon_types()
 
     assert session.query(tables.osm_polygon).get(1).type == 'city'
+
+
+def test_polygon_type_is_not_updated_when_type_not_administrative(session, tables):
+    session.add(tables.osm_polygon(id=1, country_code='ch', admin_level=8, type='city'))
+    session.add(tables.admin_level_type_mapping(country_code='ch', admin_level=8, type='state'))
+    session.commit()
+
+    set_polygon_types()
+
+    assert session.query(tables.osm_polygon).get(1).type == 'city'
