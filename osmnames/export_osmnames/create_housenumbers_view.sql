b0528b64ec8fddd3d8c7c79c3478ebb8283ae90a
Philipp <philippks@protonmail.com>
2019-05-01 17:49:13 +0200

    reduced precision of output coordinates to 5 decimals places

--- osmnames/export_osmnames/create_housenumbers_view.sql
+++ osmnames/export_osmnames/create_housenumbers_view.sql
@@ -5,7 +5,7 @@ SELECT
   street_id::VARCHAR AS street_id,
   street,
   housenumber,
-  round(ST_X(ST_Centroid(ST_Transform(geometry, 4326)))::numeric, 7) AS lon,
-  round(ST_Y(ST_Centroid(ST_Transform(geometry, 4326)))::numeric, 7) AS lat
+  round(ST_X(ST_Centroid(ST_Transform(geometry, 4326)))::numeric, 5) AS lon,
+  round(ST_Y(ST_Centroid(ST_Transform(geometry, 4326)))::numeric, 5) AS lat
 FROM osm_housenumber
 WHERE street_id IS NOT NULL;
