2e395dfb35d3532106e5a27dbd5a78110709f295
Stefan Breunig <stefan-github@yrden.de>
2023-03-11 08:19:27 +0100

    automatically setup pgadmin with all necessary credentials
    This is insecure, but more convenient when starting from scratch.
    Given pgadmin is not run by default, and it's for local use only,
    this seems acceptable.

---
+++ data/pgadmin/config.py
@@ -0,0 +1,5 @@
+SERVER_MODE = False
+SQLITE_PATH = '/var/lib/pgadmin/pgadmin4.db'
+SESSION_DB_PATH = '/var/lib/pgadmin/sessions'
+STORAGE_DIR = '/var/lib/pgadmin/storage'
+MASTER_PASSWORD_REQUIRED = False
