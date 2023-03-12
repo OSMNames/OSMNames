d4b4ebccad53202dd085311724e64944c0023895
Stefan Andres Charsley <charsleysa@gmail.com>
2023-03-12 23:08:29 +0000

    fix: update tests

--- tests/prepare_data/test_set_polygon_types.py
+++ tests/prepare_data/test_set_polygon_types.py
@@ -22,13 +22,13 @@ def test_polygon_type_is_not_updated_when_type_not_administrative(session, table
 
 
 def test_polygon_type_is_updated_correctly_when_mapping_override_exists(session, tables):
-    # Polygons use negative IDs but the mapping override table stores them postively
-    # so this test needs to use negative IDs in the polygon table otherwise it wont match
-    session.add(tables.osm_polygon(id=-2094141, country_code='nz', admin_level=6, type='administrative'))
+    # Polygons use negative osm IDs but the mapping override table stores them postively
+    # so this test needs to use negative osm IDs in the polygon table otherwise it wont match
+    session.add(tables.osm_polygon(id=1, osm_id=-2094141, country_code='nz', admin_level=6, type='administrative'))
     session.add(tables.admin_level_type_mapping(country_code='nz', admin_level=6, type='state_district'))
     session.add(tables.admin_level_type_mapping_override(country_code='nz', osm_id=2094141, type='city'))
     session.commit()
 
     set_polygon_types()
 
-    assert session.query(tables.osm_polygon).get(-2094141).type == 'city'
+    assert session.query(tables.osm_polygon).get(1).type == 'city'
