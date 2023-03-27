5dac22b6abb11429577fefa1bd6d1b1e6c7acbf7
Stefan Breunig <stefan-github@yrden.de>
2023-03-27 20:05:30 +0200

    parallelize `normalize_street_names.sql` using `auto_module.sql`
    for germany.osm.pbf:
    
    ```
    // settings
    System cpu_count=8, PostgreSQL max_connections=14, using 3 connections to parallelize auto_modulo UPDATE queries
    // master
    finished executing sql file normalize_street_names.sql (took 324.9s)
    // branch
    finished executing sql file normalize_street_names.sql (took 177.0s)
    ```
    
    `auto_modulo` does have an impact, i.e. the speedup isn't solely from
    enabling `Parallelize=True` which was forgotten. I tested this with the
    smaller dataset for Hamburg metropolitan area:
    
    ```
    sequential:   56s
    parallelized: 37s
    par+automod:  29s
    ```

--- osmnames/prepare_data/prepare_housenumbers/normalize_street_names.sql
+++ osmnames/prepare_data/prepare_housenumbers/normalize_street_names.sql
@@ -1,2 +1,2 @@
-UPDATE osm_housenumber SET normalized_street = normalize_string(street); --&
-UPDATE osm_linestring SET normalized_name = normalize_string(name); --&
+UPDATE osm_housenumber SET normalized_street = normalize_string(street) WHERE auto_modulo(id); --&
+UPDATE osm_linestring SET normalized_name = normalize_string(name) WHERE auto_modulo(id); --&
